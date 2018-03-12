9c48330ee6163190d14ffb994441e1d9c568b274
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2018-03-12 10:59:00 -0300

    Adicionada novas constantes

--- app/src/main/java/br/com/prot/orm/util/Constants.java
+++ app/src/main/java/br/com/prot/orm/util/Constants.java
@@ -4,10 +4,13 @@ package br.com.prot.orm.util;
  * Created by fredericom on 09/03/2018.
  */
 
-public class Constatnts {
+public class Constants {
 
     public static final String MSG_EMPTY_REGISTERS = "Não existem clientes cadastrados...";
-    public static final String MSG_COMENTARIOS = "escreva aqui o seu comentário...";
+    public static final String MSG_SAVE_SUCESS = "Registro salvo com sucesso!";
+    public static final String ERR_UPDATE = "Não foi possível salvar o comentário...";
+    public static final String HINT_INPUT_NOME = "Informe o nome.";
+    public static final String HINT_INPUT_IDADE = "informe a idade.";
     public static final String LB_ID = "id";
     public static final String LB_NOME = "nome";
     public static final String LB_IDADE = "idade";
