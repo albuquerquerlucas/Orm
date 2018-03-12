f2ebea981a538fdca6cf1aa2896feca3715e46a9
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2018-03-12 13:41:03 -0300

    Adicção de constantes

--- app/src/main/java/br/com/prot/orm/util/Constants.java
+++ app/src/main/java/br/com/prot/orm/util/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String ERR_UPDATE = "Não foi possível salvar o comentário...";
     public static final String HINT_INPUT_NOME = "Informe o nome.";
     public static final String HINT_INPUT_IDADE = "informe a idade.";
+    public static final String HINT_INPUT_EMAIL = "informe o e-mail.";
     public static final String LB_ID = "id";
     public static final String LB_NOME = "nome";
     public static final String LB_IDADE = "idade";
